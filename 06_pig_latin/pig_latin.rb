26c1f0b9ed489b59a49b99a13a1b757bfe6eceec
handacg <handa.cheng@gmail.com>
2018-10-08 23:50:09 +0200

    first commit

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
 #write your code here
+#write your code here
+def translate (string)
+	result = []
+	array = string.split (" ")
+	array.each do |x|
+		if x.start_with?("a","e","i","o","u","y")
+			result << x + "ay"
+		else
+			
+		end
+	end
+	return result.join(" ")
+end
